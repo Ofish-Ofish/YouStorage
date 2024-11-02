4850edeec4de968dfc9e1373c18f612840d333de
Ofish <vc.ofir@gmail.com>
2024-11-01 21:37:17 -0400

    basic encode/decode

--- YouStorage.py
+++ YouStorage.py
@@ -1,4 +1,17 @@
-with open("test.txt", "r", encoding='utf-8') as f:
-  text = f.read()
-  binary_text = ' '.join(format(byte, '08b') for char in text for byte in char.encode('utf-8'))
-  print(binary_text)
+def encode(filedir):
+  with open(filedir, "r", encoding='utf-8') as f:
+    text = f.read()
+    return ' '.join(format(byte, '08b') for char in text for byte in char.encode('utf-8'))
+
+def decode(binary):
+  byte_list = []
+  for b  in binary.split():
+    byte_list.append(int(b, 2))
+
+  return bytes(byte_list).decode('utf-8')
+
+
+if __name__ == "__main__":
+    FILEDIR = "test.txt"
+    print(encode(FILEDIR))
+    print(decode(encode(FILEDIR)))
